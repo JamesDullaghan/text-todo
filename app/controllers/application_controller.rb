d5a1604ed9ddb559700ed9d0e5c835441eb6d939
James Dullaghan <admin@knucks.io>
2021-05-02 21:42:38 -0700

    Add ping routes to check in papertrail if the application is logging to papertrail

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def pong
+    render plain: 'pong'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'application#pong'
+
   resources :text_messages, only: [:create]
+  get 'ping' => 'application#pong', as: :pong
 end
